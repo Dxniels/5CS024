0640ebf856c2e6c48445f4ad8d5ab304e23eadc3
Daniels <buholcs2004@gmail.com>
2024-02-25 11:10:16 +0000

    Slight changes / Added a toast changed service to contact instead.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 5CS024
 Website. 
+
+
+# Important Websites to keep in mind 
+
+https://getbootstrap.com/docs/5.3/examples/
+
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+// Preloader
+
 const spinnerWrapperEl = document.querySelector('.spinner-wrapper');
 
 window.addEventListener('load',() => {
@@ -7,3 +11,30 @@ window.addEventListener('load',() => {
         spinnerWrapperEl.style.display = 'none';
     }, 200);
 });
+
+
+
+// Toast 
+
+const myToast = new bootstrap.Toast('.toast')
+
+setTimeout (() => {
+    myToast.show();
+}, 10000);
+
+
+// For the date and time to show on the toast
+
+`use strict`
+var datetime = new Date();
+console.log(datetime);
+document.getElementById("time").textContent = datetime;
+
+`use strict`;
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1000);
